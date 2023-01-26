4bcadb272d727b4320915028b59fc921e8523b9c
sriramk7-hk <sriramkesavan44@gmail.com>
2023-01-26 20:23:45 +0530

    Second Commit

---
+++ contarcts/NftMarketPlace.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+error NftMarketplace__PriceMustBeAboveZero();
+
+contract NftMarketplace {
+
+    function listItem(address nftAddress, uint256 tokenId, uint256 price) external {
+        if(price <= 0){
+            revert NftMarketplace__PriceMustBeAboveZero();
+        }
+    }
+
+}
